2b9e3a6c9bcc0aba216f7eae637def4396793148
Nick Konkov <kharkivnick@gmail.com>
2025-11-26 13:05:15 +0200

    Add index func in views.py

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, DishType, Dish
+
+
+def index(request: HttpRequest):
+    num_cooks = get_user_model().objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+    }
+    return render(request, "kitchen/index.html", context=context)
