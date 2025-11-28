c1a075d1a6d159243f7d581891b12259942709ce
Nick Konkov <kharkivnick@gmail.com>
2025-11-28 18:13:50 +0200

    Add 'Login_required' to all views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import generic
 
 from kitchen.models import Cook, DishType, Dish
 
-
+@login_required(login_url="login")
 def index(request: HttpRequest):
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
@@ -20,28 +22,28 @@ def index(request: HttpRequest):
     return render(request, "kitchen/index.html", context=context)
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     queryset = Cook.objects.all().order_by("last_name")
     paginate_by = 5
 
 
-class CookDetailView(generic.DetailView):
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Cook
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.all().order_by("name")
     paginate_by = 5
     context_object_name = "dish_list"
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     queryset = DishType.objects.all().order_by("name")
     paginate_by = 5
