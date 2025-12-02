c82ba23b7961883df2f52e6b434e6e0e2ed3a9e9
Nick Konkov <kharkivnick@gmail.com>
2025-12-02 19:45:25 +0200

    Add paths for CRUD content

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -6,7 +6,18 @@ from .views import (
     CookDetailView,
     DishListView,
     DishDetailView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+    CookCreateView,
+    CookUpdateView,
+    CookDeleteView,
     DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+    remove_cook,
+    assign_cook,
 )
 
 
@@ -14,9 +25,20 @@ urlpatterns = [
     path("", index, name="index"),
     path("cook/", CookListView.as_view(), name="cook-list"),
     path("cook/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    path("cook/create", CookCreateView.as_view(), name="cook-create"),
+    path("cook/<int:pk>/update/", CookUpdateView.as_view(), name="cook-update"),
+    path("cook/<int:pk>/delete/", CookDeleteView.as_view(), name="cook-delete"),
     path("dish/", DishListView.as_view(), name="dish-list"),
     path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/create", DishCreateView.as_view(), name="dish-create"),
+    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
+    path("dish/<int:pk>/remove/", remove_cook, name="remove-cook"),
+    path("dish/<int:pk>/assign/", assign_cook, name="assign-cook"),
     path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-type/create", DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish-type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish-type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
 ]
 
 app_name = "kitchen"
